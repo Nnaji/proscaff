eeaccd868fa8165812aca099a59379f41a46562c
Kingsley Nnaji <kingsley.nnaji@gmail.com>
2019-09-05 16:20:48 +0200

    init configuration completed

--- test/test.spec.js
+++ test/test.spec.js
@@ -1,3 +1,8 @@
-const expect = require('chai').expect();
-
-describe('Main Project', () => {});
+const expect = require('chai').expect;
+describe('Main Project', () => {
+  describe('Truthy Value', () => {
+    it('should test for equality that returns true', () => {
+      expect(1).to.be.equal(1);
+    });
+  });
+});
