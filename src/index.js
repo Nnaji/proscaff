6b26ac419692520b1ede8efdc9a56a43cb4eb5ed
Kingsley Nnaji <kingsley.nnaji@gmail.com>
2019-09-08 23:15:19 +0200

    Program startup info created

--- src/index.js
+++ src/index.js
@@ -1,1 +1,20 @@
-console.log('Hello World')
+#!/usr/bin/env node
+/**************************************************************
+	@AUTHOR: Kingsley Nnaji <kingsley.nnaji@gmail.com>
+	@LICENSE: MIT
+	Creation Date : 08.09.2019
+***************************************************************/
+const program = require('commander');
+const moment = require('moment');
+const shj = require('shelljs');
+
+let userGreeting = 'Welcome'.toUpperCase();
+let programmVersion = 'Version: 1.0.0'.toUpperCase();
+
+shj.echo('******************************************');
+shj.echo('*    ', userGreeting, process.env.USER.replace('k', 'K'), ' **');
+shj.echo('*    ', 'PROSCAFF: ', programmVersion, ' **');
+shj.echo('*    ', moment().format('LLLL'), '  **');
+shj.echo('*****************************************');
+
+program.version(programmVersion);
